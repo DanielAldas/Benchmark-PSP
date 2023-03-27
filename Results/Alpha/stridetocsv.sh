50c7d2b26eb4d9ff943842252a15277502857457
Daniel Aldas Bulos <54075272+DanielAldas@users.noreply.github.com>
2023-03-27 00:35:10 -0500

    Delete Results/Alpha directory

--- Results/Alpha/stridetocsv.sh
+++
@@ -1,8 +0,0 @@
-for i in $(wc -l *summary| grep -w 2|awk '{print $2}');\
-do #code=$(awk '{print $NF}' $i | sed -n 1p); \
-	code=$(echo $i | cut -d . -f 1);  
-	length=$(awk '{print $0}' $i | sed -n 1p| sed 's/\(.*\)          /\1\t/'| awk -F"\t" '{print $1}'| awk '{print $NF}'); \
-	seq=$(sed -n 1p $i | awk '{print $3}'); \
-	structure=$(cat $i| sed -n 2p| sed 's/       /\t/'| awk -F"\t" '{print $2}'| sed -e 's/ /C/g' -e 's/T/C/g' -e 's/S/C/g' -e 's/G/H/g' -e 's/I/H/g' -e 's/b/E/g' -e 's/B/E/g' | cut -c -${length}); \
-	echo -e $code'\t'$length'\t'$seq'\t'$structure;\
-done 
--- Results/Alpha/summary.sh
+++
@@ -1,4 +0,0 @@
-for file in *.stride
-do 
-	grep -n -A1 SEQ $file > $file.summary
-done
